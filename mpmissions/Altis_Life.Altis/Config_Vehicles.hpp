4d39b8b0bbee397c57d469a50fcb9f58d1a37686
Mateuus <mateuus27@outlook.com>
2016-06-12 23:09:46 -0300

    AGORA FUNFA ESSE MERDA

--- mpmissions/Altis_Life.Altis/Config_Vehicles.hpp
+++ mpmissions/Altis_Life.Altis/Config_Vehicles.hpp
@@ -76,7 +76,7 @@ class CarShops {
 			{ "C_Hatchback_01_F", phatchback, "driver" }, 
 			{ "C_Offroad_01_F", poffroad, "driver" },
 			{ "C_SUV_01_F", psuv, "driver" },
-			{ "C_Hatchback_01_sport_F", phatchbacksport, "driver" }
+			{ "C_Hatchback_01_sport_F", phatchbacksport, "driver" },
 			{ "I_MRAP_03_F", huntervip4, "driver" { "life_donator", "SCALAR", 4 } }
 		};
 	};
